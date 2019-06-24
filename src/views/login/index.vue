c5f1744d99287f051d0f6e50fa18d88899ca8e2e
waitfor0311 <waitfor930311@outlook.com>
2019-06-24 18:19:15 +0800

    用户登陆-获取验证码-定时器处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,18 +5,31 @@
       <div class="form-head">
          <img src="./logo_index.png" alt="黑马头条号">
       </div>
-      <el-form class="login-form" ref="form" :model="form">
-        <el-form-item>
+      <el-form
+        class="login-form"
+        ref="form"
+        :model="form"
+        :rules="rules"
+      >
+        <el-form-item prop="mobile">
           <el-input v-model="form.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <!-- el-col 栅格布局。一共24 列：span 用来指定占用大小 :offset 用来指定偏移量 -->
           <el-col :span="14">
             <el-input v-model="form.code" placeholder="请输入验证码"></el-input>
           </el-col>
           <el-col :offset="1" :span="9">
-            <el-button @click="handleSendCode">获取验证码</el-button>
+            <!-- <el-button @click="handleSendCode">获取验证码</el-button> -->
+          <el-button
+            @click="handleSendCode"
+            :disabled="!!codeTimer"
+            > {{ codeTimer ? `剩余${codeTimeSeconds}秒`:'获取验证码' }}</el-button>
           </el-col>
+        </el-form-item>
+        <el-form-item prop="agree">
+         <el-checkbox class="agree-checkbox" v-model="form.agree"></el-checkbox>
+         <span class="agree-text">我已阅读并同意<a href="#">用户协议</a>和<a href="#">隐私条款</a></span>
         </el-form-item>
         <el-form-item>
           <el-button class="btn-login" type="primary" @click="handleLogin">登录</el-button>
@@ -28,19 +41,46 @@
 <script>
 import axios from 'axios'
 import '@/vendor/gt'
+const initCodeTimeseconds = 5
 export default {
   name: 'AppLogin',
   data () {
     return {
-      form: {
+      form: { // 表单数据对象
         mobile: '',
-        code: ''
-      }
+        code: '',
+        agree: ''
+      },
+      rules: { // 验证规则对象
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          // { len: 11, message: '长度必须11位', trigger: 'blur' }
+          { pattern: /\d{11}/, message: '请输入有效的手机号', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          // { len: 6, message: '长度必须6位', trigger: 'blur' }
+          { pattern: /\d{6}/, message: '请输入有效的验证码', trigger: 'blur' }
+        ],
+        agree: [
+          { required: true, message: '请同意用户协议' },
+          { pattern: /true/, message: '请同意用户协议' }
+        ]
+      },
+      codeTimer: null, // 倒计时定时器
+      codeTimeSeconds: initCodeTimeseconds // 倒计时事件
     }
   },
   methods: {
     handleLogin () {
-      // const { mobile, code } = this.form
+      this.$refs['form'].validate(valid => {
+        if (!valid) {
+          return
+        }
+        this.submitLogin()
+      })
+    },
+    submitLogin () {
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
@@ -48,7 +88,7 @@ export default {
       }).then(res => {
         // console.log(res.data)
         this.$message({
-          message: '恭喜成功',
+          message: '登录成功',
           type: 'success'
         })
         this.$router.push({
@@ -60,6 +100,19 @@ export default {
         })
     },
     handleSendCode () {
+      // 验证手机号是否有效
+      this.$refs['form'].validateField('mobile', errorMessage => {
+        if (errorMessage.trim().length > 0) {
+          return
+        }
+        // 验证通过，初始化显示验证码
+        this.showGeetest()
+      })
+    },
+    /**
+     * 验证通过，初始化显示人机交互验证码 */
+    showGeetest () {
+      // 函数中的function内部的this指向window
       const { mobile } = this.form
       axios({
         method: 'GET',
@@ -72,11 +125,11 @@ export default {
           offline: !data.success,
           new_captcha: data.new_captcha,
           product: 'bind' // 隐藏，直接弹出
-        }, function (captchaObj) {
-          captchaObj.onReady(function () {
+        }, captchaObj => {
+          captchaObj.onReady(() => {
           // 验证码ready之后才能调用verify方法显示验证码
             captchaObj.verify() // 弹出验证内容框
-          }).onSuccess(function () {
+          }).onSuccess(() => {
           // your code
             const {
               geetest_challenge: challenge,
@@ -91,7 +144,7 @@ export default {
                 seccode
               }
             }).then(res => {
-              console.log(res.data)
+              this.codeCountDown()
             })
           }).onError(function () {
           // your code
@@ -99,6 +152,20 @@ export default {
           // 在这里注册”发送验证码“ 的按钮点击事件
         })
       })
+    },
+    /**
+     * 验证码倒计时
+     */
+    codeCountDown () {
+      this.codeTimer = window.setInterval(() => {
+        this.codeTimeSeconds--
+        if (this.codeTimeSeconds <= 0) {
+          // 清除定时器
+          window.clearInterval(this.codeTimer)
+          this.codeTimeSeconds = initCodeTimeseconds
+          this.codeTimer = null // 讲存储的定时器引用的变量重新赋值为null
+        }
+      }, 1000)
     }
   }
 }
